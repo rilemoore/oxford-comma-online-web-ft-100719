f9ade78ac506d219f7edfb7e85e9585b9e821e3b
Riley Moore <rilemoore@gmail.com>
2019-10-14 16:57:30 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,14 +7,15 @@ def oxford_comma(array)
   if(array.size == 2)
     
       return array.join(" and ")
+      
   else
    while count < array.size
     if(count == array.size - 1)
-      string << "and" << array[count] << ", "
+      string << "and " << array[count]
     else
-    
+      string << array[count] << ", "
+    end
    end
   end
-  array.join(" and ")
   
 end
